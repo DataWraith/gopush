b680cec28ac806151710c3367f11666f7e784192
Johannes Holzfuß <DataWraith@web.de>
2014-03-15 17:52:00 +0100

    Implement the instructions for the boolean stack

--- gopush.go
+++ gopush.go
@@ -3,6 +3,7 @@ package gopush
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 	"unicode"
@@ -38,8 +39,57 @@ func (s *Stack) Pop() (item interface{}) {
 	return item
 }
 
-func (s Stack) Len() int {
-	return len(s.Stack)
+func (s Stack) Len() int64 {
+	return int64(len(s.Stack))
+}
+
+func (s *Stack) Dup() {
+	if len(s.Stack) == 0 {
+		return
+	}
+
+	s.Push(s.Peek())
+}
+
+func (s *Stack) Swap() {
+	if len(s.Stack) < 2 {
+		return
+	}
+
+	i1 := s.Pop()
+	i2 := s.Pop()
+	s.Push(i1)
+	s.Push(i2)
+}
+
+func (s *Stack) Flush() {
+	s.Stack = nil
+}
+
+func (s *Stack) Rot() {
+	if len(s.Stack) < 3 {
+		return
+	}
+
+	i1 := s.Pop()
+	i2 := s.Pop()
+	i3 := s.Pop()
+
+	s.Push(i2)
+	s.Push(i1)
+	s.Push(i3)
+}
+
+func (s *Stack) Shove(item interface{}, idx int64) {
+	//TODO
+}
+
+func (s *Stack) Yank(idx int64) {
+	// TODO
+}
+
+func (s *Stack) YankDup(idx int64) {
+	// TODO
 }
 
 func NewIntStack(options Options) *Stack {
@@ -113,6 +163,66 @@ func NewBooleanStack(options Options) *Stack {
 		Functions: make(map[string]Instruction),
 	}
 
+	s.Functions["="] = func(stacks map[string]*Stack) {
+		if stacks["boolean"].Len() < 2 {
+			return
+		}
+
+		b1 := stacks["boolean"].Pop().(bool)
+		b2 := stacks["boolean"].Pop().(bool)
+		stacks["boolean"].Push(b1 == b2)
+	}
+
+	s.Functions["and"] = func(stacks map[string]*Stack) {
+		if stacks["boolean"].Len() < 2 {
+			return
+		}
+
+		b1 := stacks["boolean"].Pop().(bool)
+		b2 := stacks["boolean"].Pop().(bool)
+		stacks["boolean"].Push(b1 && b2)
+	}
+
+	s.Functions["define"] = func(stacks map[string]*Stack) {
+		// TODO
+		return
+	}
+
+	s.Functions["dup"] = func(stacks map[string]*Stack) {
+		stacks["boolean"].Dup()
+	}
+
+	s.Functions["flush"] = func(stacks map[string]*Stack) {
+		stacks["boolean"].Flush()
+	}
+
+	s.Functions["fromfloat"] = func(stacks map[string]*Stack) {
+		if stacks["float"].Len() == 0 {
+			return
+		}
+
+		f := stacks["float"].Pop().(float64)
+		stacks["boolean"].Push(f != 0)
+	}
+
+	s.Functions["frominteger"] = func(stacks map[string]*Stack) {
+		if stacks["integer"].Len() == 0 {
+			return
+		}
+
+		i := stacks["integer"].Pop().(int64)
+		stacks["boolean"].Push(i != 0)
+	}
+
+	s.Functions["not"] = func(stacks map[string]*Stack) {
+		if stacks["boolean"].Len() == 0 {
+			return
+		}
+
+		b := stacks["boolean"].Pop().(bool)
+		stacks["boolean"].Push(!b)
+	}
+
 	s.Functions["or"] = func(stacks map[string]*Stack) {
 		if stacks["boolean"].Len() < 2 {
 			return
@@ -121,6 +231,50 @@ func NewBooleanStack(options Options) *Stack {
 		b1 := stacks["boolean"].Pop().(bool)
 		b2 := stacks["boolean"].Pop().(bool)
 		stacks["boolean"].Push(b1 || b2)
+	}
+
+	s.Functions["pop"] = func(stacks map[string]*Stack) {
+		stacks["boolean"].Pop()
+	}
+
+	s.Functions["rand"] = func(stacks map[string]*Stack) {
+		stacks["boolean"].Push(rand.Float64() < 0.5)
+	}
+
+	s.Functions["rot"] = func(stacks map[string]*Stack) {
+		stacks["boolean"].Rot()
+	}
+
+	s.Functions["shove"] = func(stacks map[string]*Stack) {
+		if stacks["boolean"].Len() == 0 || stacks["integer"].Len() == 0 {
+			return
+		}
+
+		stacks["boolean"].Shove(stacks["boolean"].Pop(), stacks["integer"].Pop().(int64))
+	}
+
+	s.Functions["stackdepth"] = func(stacks map[string]*Stack) {
+		stacks["integer"].Push(stacks["boolean"].Len())
+	}
+
+	s.Functions["swap"] = func(stacks map[string]*Stack) {
+		stacks["boolean"].Swap()
+	}
+
+	s.Functions["yank"] = func(stacks map[string]*Stack) {
+		if stacks["integer"].Len() == 0 {
+			return
+		}
+
+		stacks["boolean"].Yank(stacks["integer"].Pop().(int64))
+	}
+
+	s.Functions["yankdup"] = func(stacks map[string]*Stack) {
+		if stacks["integer"].Len() == 0 {
+			return
+		}
+
+		stacks["boolean"].YankDup(stacks["integer"].Pop().(int64))
 	}
 
 	return s
