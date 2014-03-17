7d0571d74aa42793949d424d54a3cdcb6378b928
Johannes Holzfuß <DataWraith@web.de>
2014-03-17 17:30:34 +0100

    Implement most of the functions of the INTEGER stack

--- intstack.go
+++ intstack.go
@@ -7,6 +7,20 @@ func NewIntStack(interpreter *Interpreter) *Stack {
 		Functions: make(map[string]Instruction),
 	}
 
+	s.Functions["%"] = func() {
+		// TODO
+	}
+
+	s.Functions["*"] = func() {
+		if !interpreter.stackOK("integer", 2) {
+			return
+		}
+
+		i1 := interpreter.Stacks["integer"].Pop().(int64)
+		i2 := interpreter.Stacks["integer"].Pop().(int64)
+		interpreter.Stacks["integer"].Push(i1 * i2)
+	}
+
 	s.Functions["+"] = func() {
 		if !interpreter.stackOK("integer", 2) {
 			return
@@ -27,14 +41,14 @@ func NewIntStack(interpreter *Interpreter) *Stack {
 		interpreter.Stacks["integer"].Push(i2 - i1)
 	}
 
-	s.Functions["*"] = func() {
-		if !interpreter.stackOK("integer", 2) {
+	s.Functions["/"] = func() {
+		if !interpreter.stackOK("integer", 2) || interpreter.Stacks["integer"].Peek().(int64) == 0 {
 			return
 		}
 
 		i1 := interpreter.Stacks["integer"].Pop().(int64)
 		i2 := interpreter.Stacks["integer"].Pop().(int64)
-		interpreter.Stacks["integer"].Push(i1 * i2)
+		interpreter.Stacks["integer"].Push(i2 / i1)
 	}
 
 	s.Functions["<"] = func() {
@@ -57,6 +71,16 @@ func NewIntStack(interpreter *Interpreter) *Stack {
 		interpreter.Stacks["boolean"].Push(i2 == i1)
 	}
 
+	s.Functions[">"] = func() {
+		if !interpreter.stackOK("integer", 2) || !interpreter.stackOK("boolean", 0) {
+			return
+		}
+
+		i1 := interpreter.Stacks["integer"].Pop().(int64)
+		i2 := interpreter.Stacks["integer"].Pop().(int64)
+		interpreter.Stacks["boolean"].Push(i2 > i1)
+	}
+
 	s.Functions["define"] = func() {
 		if !interpreter.stackOK("name", 1) || !interpreter.stackOK("integer", 1) {
 			return
@@ -72,6 +96,32 @@ func NewIntStack(interpreter *Interpreter) *Stack {
 		interpreter.Stacks["integer"].Dup()
 	}
 
+	s.Functions["flush"] = func() {
+		interpreter.Stacks["integer"].Flush()
+	}
+
+	s.Functions["fromboolean"] = func() {
+		if !interpreter.stackOK("boolean", 1) {
+			return
+		}
+
+		b := interpreter.Stacks["boolean"].Pop().(bool)
+		if b {
+			interpreter.Stacks["integer"].Push(int64(1))
+		} else {
+			interpreter.Stacks["integer"].Push(int64(0))
+		}
+	}
+
+	s.Functions["fromfloat"] = func() {
+		if !interpreter.stackOK("float", 1) {
+			return
+		}
+
+		f := interpreter.Stacks["float"].Pop().(float64)
+		interpreter.Stacks["integer"].Push(int64(f))
+	}
+
 	s.Functions["max"] = func() {
 		if !interpreter.stackOK("integer", 2) {
 			return
@@ -79,7 +129,23 @@ func NewIntStack(interpreter *Interpreter) *Stack {
 
 		i1 := interpreter.Stacks["integer"].Pop().(int64)
 		i2 := interpreter.Stacks["integer"].Pop().(int64)
+
 		if i1 > i2 {
+			interpreter.Stacks["integer"].Push(i1)
+		} else {
+			interpreter.Stacks["integer"].Push(i2)
+		}
+	}
+
+	s.Functions["min"] = func() {
+		if !interpreter.stackOK("integer", 2) {
+			return
+		}
+
+		i1 := interpreter.Stacks["integer"].Pop().(int64)
+		i2 := interpreter.Stacks["integer"].Pop().(int64)
+
+		if i1 < i2 {
 			interpreter.Stacks["integer"].Push(i1)
 		} else {
 			interpreter.Stacks["integer"].Push(i2)
@@ -88,6 +154,52 @@ func NewIntStack(interpreter *Interpreter) *Stack {
 
 	s.Functions["pop"] = func() {
 		interpreter.Stacks["integer"].Pop()
+	}
+
+	s.Functions["rand"] = func() {
+		// TODO
+	}
+
+	s.Functions["rot"] = func() {
+		interpreter.Stacks["integer"].Rot()
+	}
+
+	s.Functions["shove"] = func() {
+		if !interpreter.stackOK("integer", 2) {
+			return
+		}
+
+		i1 := interpreter.Stacks["integer"].Pop().(int64)
+		i2 := interpreter.Stacks["integer"].Peek().(int64)
+
+		interpreter.Stacks["integer"].Shove(i2, i1)
+		interpreter.Stacks["integer"].Pop()
+	}
+
+	s.Functions["stackdepth"] = func() {
+		interpreter.Stacks["integer"].Push(interpreter.Stacks["integer"].Len())
+	}
+
+	s.Functions["swap"] = func() {
+		interpreter.Stacks["integer"].Swap()
+	}
+
+	s.Functions["yank"] = func() {
+		if !interpreter.stackOK("integer", 2) {
+			return
+		}
+
+		idx := interpreter.Stacks["integer"].Pop().(int64)
+		interpreter.Stacks["integer"].Yank(idx)
+	}
+
+	s.Functions["yankdup"] = func() {
+		if !interpreter.stackOK("integer", 2) {
+			return
+		}
+
+		idx := interpreter.Stacks["integer"].Pop().(int64)
+		interpreter.Stacks["integer"].YankDup(idx)
 	}
 
 	return s
