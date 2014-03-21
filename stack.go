a3d9ae415a76af18ea94de52c76a1078dd948e9e
Johannes Holzfuß <DataWraith@web.de>
2014-03-21 21:47:56 +0100

    Implement YANKDUP

--- stack.go
+++ stack.go
@@ -100,5 +100,17 @@ func (s *Stack) Yank(idx int64) {
 }
 
 func (s *Stack) YankDup(idx int64) {
-	// TODO
+	if len(s.Stack) == 0 {
+		return
+	}
+
+	index := int64(len(s.Stack)-1) - idx
+	if index < 0 {
+		index = 0
+	} else if index > int64(len(s.Stack)-1) {
+		index = int64(len(s.Stack) - 1)
+	}
+
+	item := s.Stack[index]
+	s.Stack = append(s.Stack, item)
 }
