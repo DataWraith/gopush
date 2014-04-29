36bd7830e564e384a9441a0df5c99bd51ad9ceb8
Johannes Holzfuß <DataWraith@web.de>
2014-04-29 18:44:54 +0200

    Improve doc.go

--- doc.go
+++ doc.go
@@ -26,12 +26,30 @@ After creating your new data type, you need to register it with Interpreter to
 make it usable.
 
 	stackName := "print"
-	interpreter.RegisterStack(stackName, printStack);
+	interpreter.Options.RegisterStack(stackName, printStack)
+	interpreter.RegisterStack(stackName, printStack)
+
+The first RegisterStack call adds all instructions from printStack to the list
+of allowed instructions. The second RegisterStack call adds printStack and its
+instructions to the Interpreter so they can be used. This double-registration is
+not ideal, and I would welcome suggestions on how to remedy it without losing
+the ability to mark certain instructions as disallowed.
 
 Finally, you can run the interpreter to execute a given program:
 
 	program := "PRINT.HELLO"
 	interpreter.Run(program)
+
+Alternatively you can parse the program into the Code representation and run
+that:
+
+	program := "PRINT.HELLO"
+	c, err := ParseCode(program)
+	if err != nil {
+		// handle error
+	}
+
+	interpreter.RunCode(c)
 
 */
 package gopush
